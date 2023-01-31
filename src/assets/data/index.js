0ff4bb7c9f86f898de21fbd4430a2638f39cba13
davidecardelli <davide.cardelli.94@gmail.com>
2023-01-31 18:49:22 +0100

    move static data into index.js

--- src/assets/data/index.js
+++ src/assets/data/index.js
@@ -1,1 +1,42 @@
 // Creao questo index.js con i miei dati statici
+const headerMenu = [
+    {
+        id: 0,
+        text: 'Home',
+        url: '#',
+        current: true,
+        isNew: false,
+    }, {
+        id: 1,
+        text: 'Services',
+        url: '#',
+        current: false,
+        isNew: false,
+    }, {
+        id: 2,
+        text: 'About',
+        url: '#',
+        current: false,
+        isNew: false,
+    }, {
+        id: 3,
+        text: 'Videos',
+        url: '#',
+        current: false,
+        isNew: false,
+    }, {
+        id: 4,
+        text: 'Blog',
+        url: '#',
+        current: false,
+        isNew: false,
+    }, {
+        id: 5,
+        text: 'Store',
+        url: '#',
+        current: false,
+        isNew: true,
+    },
+];
+
+export { headerMenu }
--- src/components/MacroComponents/AppHeader.vue
+++ src/components/MacroComponents/AppHeader.vue
@@ -1,49 +1,11 @@
 <script>
+import { headerMenu } from '../../assets/data';
 import Logo from '../MicroComponents/Logo.vue';
 export default {
     name: 'AppHeader',
     components: { Logo },
     data() {
-        return {
-            heaederMenu: [
-                {
-                    text: 'Home',
-                    url: '#',
-                    current: true,
-                    isNew: false,
-                },
-                {
-                    text: 'Services',
-                    url: '#',
-                    current: false,
-                    isNew: false,
-                },
-                {
-                    text: 'About',
-                    url: '#',
-                    current: false,
-                    isNew: false,
-                },
-                {
-                    text: 'Videos',
-                    url: '#',
-                    current: false,
-                    isNew: false,
-                },
-                {
-                    text: 'Blog',
-                    url: '#',
-                    current: false,
-                    isNew: false,
-                },
-                {
-                    text: 'Store',
-                    url: '#',
-                    current: false,
-                    isNew: true,
-                },
-            ]
-        }
+        return { headerMenu }
     },
     props: {
     },
@@ -61,9 +23,9 @@ export default {
             <logo></logo>
             <nav>
                 <ul class="d-flex align-items-center m-0">
-                    <li v-for="voice in this.heaederMenu">
-                        <a :href="voice.url" class="m-2" :class="{ 'selected': voice.current }">{{ voice.text }}</a>
-                        <span v-if="voice.isNew" class="is-new">new</span>
+                    <li v-for="voice in this.headerMenu">
+                        <a :href="voice.url" class="ms-3" :class="{ 'selected': voice.current }">{{ voice.text }}</a>
+                        <span v-if="voice.isNew" class="is-new ms-2">new</span>
                     </li>
                 </ul>
             </nav>
