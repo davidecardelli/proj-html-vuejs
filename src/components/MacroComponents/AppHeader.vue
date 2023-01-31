035c42034bc91df8ed2693dab8f48212d4e27a7b
davidecardelli <davide.cardelli.94@gmail.com>
2023-01-31 16:50:02 +0100

    add favicon

--- src/components/MacroComponents/AppHeader.vue
+++ src/components/MacroComponents/AppHeader.vue
@@ -1,7 +1,8 @@
 <script>
+import Logo from '../MicroComponents/Logo.vue';
 export default {
     name: 'AppHeader',
-    components: {},
+    components: { Logo },
     data() {
         return {
             heaederMenu: [
@@ -51,21 +52,18 @@ export default {
     methods: {
     },
 }
-
 </script>
 
 <template>
 
     <header>
         <div class="container d-flex justify-content-between align-items-center py-2">
-            <div id="logo">
-                <img src="../../assets/img/logo.png" alt="logo">
-            </div>
+            <logo></logo>
             <nav>
                 <ul class="d-flex align-items-center m-0">
                     <li v-for="voice in this.heaederMenu">
                         <a :href="voice.url" class="m-2" :class="{ 'selected': voice.current }">{{ voice.text }}</a>
-                        <span v-if="voice.isNew">new</span>
+                        <span v-if="voice.isNew" class="is-new">new</span>
                     </li>
                 </ul>
             </nav>
@@ -87,5 +85,13 @@ header {
     .selected {
         color: $white;
     }
+}
+
+.is-new {
+    font-size: 0.7rem;
+    padding: 2px;
+    margin: 0;
+    border-radius: 20%;
+    background-color: $yellow;
 }
 </style>
