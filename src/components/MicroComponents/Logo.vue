3f31d0a864682dd69dd87722d39f4646071de984
davidecardelli <davide.cardelli.94@gmail.com>
2023-01-31 16:45:04 +0100

    add Logo.vue

---
+++ src/components/MicroComponents/Logo.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+    name: 'Logo',
+    components: {},
+    data() {
+        return {}
+    },
+    props: {
+    },
+    computed: {
+    },
+    methods: {
+    },
+}
+
+
+</script>
+
+<template>
+    <div id="logo">
+        <img src="../../assets/img/logo.png" alt="logo">
+    </div>
+
+</template>
+
+<style lang="scss">
+
+</style>
