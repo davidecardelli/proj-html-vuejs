a32c6eef92555d333c43be93e2d0839c8daa194f
davidecardelli <davide.cardelli.94@gmail.com>
2023-02-01 11:10:49 +0100

    add Jumbotron in AppHeader

--- src/components/MacroComponents/AppHeader.vue
+++ src/components/MacroComponents/AppHeader.vue
@@ -19,7 +19,7 @@ export default {
 <template>
 
     <header>
-        <div class="container d-flex justify-content-between align-items-center py-2">
+        <div class="container d-flex justify-content-between align-items-center py-3">
             <logo></logo>
             <nav>
                 <ul class="d-flex align-items-center m-0">
@@ -30,23 +30,71 @@ export default {
                 </ul>
             </nav>
         </div>
+        <div class="jumbotron">
+            <div class="container">
+                <div class=" row row-cols-2 d-flex align-items-center justify-content-center">
+                    <div class="col-8 py-5 flex-grow-1">
+                        <h2 class="mb-4">Certified fitness professional</h2>
+                        <h1 class="mb-4">Take control of your health</h1>
+                        <i class="fa-solid fa-wave-square mb-4"></i>
+                        <p>Curabitur non nulla sit amet nisl tempus convallis quis ac lectus dolor sit amet.</p>
+                    </div>
+                    <div class="col-4 d-flex align-items-center justify-content-center">
+                        PLAY
+                    </div>
+                </div>
+            </div>
+        </div>
     </header>
 
 </template>
         
-<style lang="scss">
+<style lang="scss" scoped>
 @use'../../assets/style/partials/variables' as *;
 @use'../../assets/style/style.scss';
 
 header {
     background-color: $black;
-}
 
-.is-new {
-    font-size: 0.7rem;
-    padding: 2px;
-    margin: 0;
-    border-radius: 20%;
-    background-color: $yellow;
+    .is-new {
+        font-size: 0.7rem;
+        padding: 2px;
+        margin: 0;
+        border-radius: 20%;
+        background-color: $yellow;
+    }
+
+    .jumbotron {
+        background-image: linear-gradient(90deg, $black 45%, rgba(6, 6, 7, 0) 90%), url(../../assets/img/slider82x-scaled.jpg);
+        background-size: cover;
+        background-position: top center;
+
+        .row {
+            padding-top: 7rem;
+            padding-bottom: 7rem;
+
+            h1 {
+                color: $white;
+                font-size: 4rem;
+            }
+
+            h2 {
+                color: $red;
+                font-size: 1.5rem;
+            }
+
+            i {
+                color: $yellow;
+                font-size: 3rem;
+            }
+
+            p {
+                font-size: 1.3rem;
+                font-weight: 400;
+            }
+        }
+
+
+    }
 }
 </style>
